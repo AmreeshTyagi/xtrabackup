63c663c020177f72ff2548987042dbe7c04ad82f
Tyagi <atyagi@noi-docker-monotype.agfamonotype.org>
2018-12-17 19:48:15 +0530

    cron backup port changed to 3308

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@
 backupdir=${NODE_NAME}-`date +%F-%H-%M`
 mkdir -p /backup/$backupdir
 echo "Backup to $backupdir started"
-ncat --recv-only $NODE_NAME 3307  > /backup/$backupdir/xtrabackup.stream
+ncat --recv-only $NODE_NAME 3308  > /backup/$backupdir/xtrabackup.stream
 echo "Backup finished"
