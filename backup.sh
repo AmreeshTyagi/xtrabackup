9e1685628b5eba159918f1924bc50879b11cea72
Tyagi <atyagi@noi-docker-monotype.agfamonotype.org>
2018-12-17 19:58:32 +0530

    adding port support

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # expect NODE_NAME to take backup
+# expect NODE_PORT to connect backup service
 
 backupdir=${NODE_NAME}-`date +%F-%H-%M`
 mkdir -p /backup/$backupdir
 echo "Backup to $backupdir started"
-ncat --recv-only $NODE_NAME 3308  > /backup/$backupdir/xtrabackup.stream
+ncat --recv-only $NODE_NAME $NODE_PORT  > /backup/$backupdir/xtrabackup.stream
 echo "Backup finished"
